5b278dc3860e0764c246ddd5da192588640e7a14
jdeklijn <57561513+jdeklijn@users.noreply.github.com>
2021-03-12 09:31:22 +0100

    Les 5
    Multidemensionale arrays

--- phpfiles/registration.php
+++ phpfiles/registration.php
@@ -26,28 +26,49 @@ if(!empty($_POST)){                                                             
 }
 
 echo("<hr/>");
-// keys                 0    1   2   3
-$aDemoArray = array("String",4,true,5.4);
-var_dump($aDemoArray);
-echo("<hr/>");
-echo($aDemoArray[3]);
-echo("<hr/>");
-$aColors = array(); // Declare an empty array for the colors
-$aColors['Green'] = "groen";
-$aColors['Red'] = "rood";
-$aColors['Blue'] = "blauw";
-$aColors['Purple'] = "paars";
-echo("<hr/>");
-echo("De kleur die u kiest is: ".$aColors['Blue']);
-echo("<hr/>");
-$aFruit = array("Appel","Banaan","Sinasappel","Mango","Kiwi","Framboos");
-var_dump($aFruit);
+//// keys                 0    1   2   3
+//$aDemoArray = array("String",4,true,5.4);
+//var_dump($aDemoArray);
+//echo("<hr/>");
+//echo($aDemoArray[3]);
+//echo("<hr/>");
+//$aColors = array(); // Declare an empty array for the colors
+//$aColors['Green'] = "groen";
+//$aColors['Red'] = "rood";
+//$aColors['Blue'] = "blauw";
+//$aColors['Purple'] = "paars";
+//echo("<hr/>");
+//echo("De kleur die u kiest is: ".$aColors['Blue']);
+//echo("<hr/>");
+//$aFruit = array("Appel","Banaan","Sinasappel","Mango","Kiwi","Framboos");
+//var_dump($aFruit);
+//
+//for($iFruitCounter = 0;$iFruitCounter<=5;$iFruitCounter++){
+//  echo("Het lekkerste fruit is de :".$aFruit[$iFruitCounter]."<br/>");
+//}
 
-for($iFruitCounter = 0;$iFruitCounter<=5;$iFruitCounter++){
-  echo("Het lekkerste fruit is de :".$aFruit[$iFruitCounter]."<br/>");
-}
+$aNesting = array("key0","key1","key2","key3","key4","key5","key6");
+$aContent = array("string",2341234,true,34.123,$aNesting);
+//var_dump($aContent);
 
-
+$aVolvo = array("Volvo","XC90","XC60","V90","V60","S90");
+$aOpel = array("Opel","Corsa","Astra","Mokka","Zafira","Insignia");
+$aTesla = array("Tesla","Model X","Roadster","Model S","Model 3","Model Y");
+$aPeugot = array("Peugot","208","308","2008","508","206","608","3008","5008");
+$aSmart = array("Smart","ForTwo","ForFour","Roadster");
+$aCars = array($aVolvo,$aTesla,$aPeugot,$aOpel,$aSmart);
+//var_dump($aCars);
+echo("<hr>");
+echo("Het model van mijn keuze is: ".$aCars[1][3]."<br/>");
+foreach($aCars as $aModel){
+    echo("Automerk: ".$aModel[0]."<br/>");
+    $iModelCounter = count($aModel);
+    echo("<select name='iCarType'>");
+    for($iCarcounter = 1;$iCarcounter<$iModelCounter;$iCarcounter++){
+        echo("<option value=".$iCarcounter.">".$aModel[$iCarcounter]."</option>");
+    } // end for loop
+    echo("</select><hr/>");
+} // End foreach
 
 echo("<hr/>");
 // ================ Display the registration form =================
